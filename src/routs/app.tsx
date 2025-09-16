5c8db30f0b297149ce6afddbf52583a973728961
OpenApps <32998735+openXapps@users.noreply.github.com>
2025-09-16 09:04:53 +0200

    Progress

--- src/routs/app.tsx
+++ src/routs/app.tsx
@@ -1,23 +1,20 @@
 import { RouterProvider, createBrowserRouter } from "react-router"
 import { ThemeProvider } from "@/context/theme-provider"
 
+import Root from "@/routs/root"
 import Home from "@/components/home"
-import Layout from "@/routs/layout"
+// import Layout from "@/routs/layout"
 import NoPage from "@/routs/nopage"
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      Component: <Root />,
       children: [
         {
           index: true,
-          element: <Home />,
-        },
-        {
-          path: "/",
-          element: <Home />,
+          Component: <Home />,
         },
       ],
       errorElement: <NoPage />,
