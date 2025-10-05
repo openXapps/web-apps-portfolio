18f1f1572c84c1593fa6088caac0542b9c52faa4
OpenApps <32998735+openXapps@users.noreply.github.com>
2025-10-05 10:09:50 +0200

    Progress

--- src/components/items.tsx
+++ src/components/items.tsx
@@ -10,24 +10,32 @@ export function ItemHeader({ title }: { title: string }) {
 
 type ItemProps = {
   title?: string
-  value: string
+  value?: string
+  bullets?: {
+    product: string
+    experience: string
+  }[]
   isLink?: boolean
   showCopy?: boolean
 }
 
-export function Item({ title, value, isLink = false, showCopy = false }: ItemProps) {
+export function Item({ title = "", value = "", bullets = [], isLink = false, showCopy = false }: ItemProps) {
   return (
     <div className="text-center px-0.5 py-2 space-y-1 rounded-sm bg-gray-800">
       {title && <h5 className="text-slate-100 font-semibold">{title}</h5>}
-      {isLink ? (
-        <a className="text-slate-300" href={value} target="_blank">{value}</a>
-      ) : (
-        <h5 className="text-slate-300"
-        >{value} {showCopy && <span className="inline-flex align-text-top ml-2 hover:cursor-copy">
-          <Clipboard size={15} />
-        </span>}
-        </h5>
-      )}
+
+      {isLink && value && <a className="text-slate-300" href={value} target="_blank">{value}</a>}
+
+      {value && <h5 className="text-slate-300"
+      >{value} {showCopy && <span className="inline-flex align-text-top ml-2 hover:cursor-copy">
+        <Clipboard size={15} />
+      </span>}
+      </h5>
+      }
+
+      {bullets.length > 0 && bullets.map((v, i) => (
+        <h5 key={i}>{v.product + " " + v.experience}</h5>
+      ))}
     </div>
   )
 }
--- src/routes/skills.tsx
+++ src/routes/skills.tsx
@@ -1,11 +1,15 @@
-import { ItemHeader } from '@/components/items';
+import { Item, ItemHeader } from '@/components/items';
 
 export default function Skills() {
   return (
-      <div className="flex flex-col gap-3 w-full">
-        <ItemHeader title="BMC Helix / Remedy" />
-        <ItemHeader title="Web Development" />
-        <ItemHeader title="Data Management" />
+    <div className="flex flex-col gap-3 w-full">
+      <ItemHeader title="BMC Helix (Remedy)" />
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-2">
+        <Item title="Development" value="X" />
+        <Item value="Some text with no title Some text with no title Some text with no title Some text with no title" showCopy />
       </div>
-    )
+      <ItemHeader title="Web Development" />
+      <ItemHeader title="Data Management" />
+    </div>
+  )
 }
